694344c394dc4895548496f31523e417790c607a
Danh <danh.nghuu@gmail.com>
2024-08-15 18:10:16 +0700

    update new api

---
+++ Allora/setup-both-3workers.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+cd allora-huggingface-walkthrough/
+mkdir -p ./worker-data
+chmod +x init.config
+
+cp ./config-both.json ./config.json
+./init.config
+
+cp ./worker-data/env_file ./worker-data/env_file_1
+cp ./worker-data/env_file ./worker-data/env_file_2
+cp ./worker-data/env_file ./worker-data/env_file_3
+
+rm -rf docker-compose.yaml
+
+cat <<EOF > docker-compose.yaml
+services:
+    inference:
+        container_name: inference-hf
+        build:
+            context: .
+            dockerfile: Dockerfile
+        command: python -u /app/app.py
+        ports:
+            - "8000:8000"
+
+    worker1:
+        container_name: worker1
+        image: alloranetwork/allora-offchain-node:latest
+        volumes:
+            - ./worker-data:/data
+        depends_on:
+            - inference
+        env_file:
+            - ./worker-data/env_file_1
+
+    worker2:
+        container_name: worker2
+        image: alloranetwork/allora-offchain-node:latest
+        volumes:
+            - ./worker-data:/data
+        depends_on:
+            - inference
+        env_file:
+            - ./worker-data/env_file_2
+    
+    worker3:
+        container_name: worker3
+        image: alloranetwork/allora-offchain-node:latest
+        volumes:
+            - ./worker-data:/data
+        depends_on:
+            - inference
+        env_file:
+            - ./worker-data/env_file_3
+  
+volumes:
+    inference-data:
+    worker-data:
+EOF
